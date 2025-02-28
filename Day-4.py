66572908632b80141d113881ac33749c425b5b10
MHASSANRAZA01 <hassantazaattari257@gmail.com>
2025-03-01 00:30:53 +0500

    Updated project with new changes

---
+++ Day-4.py
@@ -0,0 +1,6 @@
+from num2words import num2words 
+
+Number = input("Enter a Number:")
+Words = num2words(int(Number))
+print("output:",Words)
+ 
---
+++ Day-5.py
@@ -0,0 +1,6 @@
+
+number = int(input("Enter a number: "))
+total_sum = 0
+for i in range(1, number + 1):
+    total_sum += i  
+print(f"The sum of numbers from 1 to {number} is: {total_sum}")
